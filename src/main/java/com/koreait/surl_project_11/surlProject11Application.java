68132b4829b16f4b6eeb6b180eab9637da463667
s3496h <s3496h@gmail.com>
2024-11-05 13:47:01 +0900

    Lombok

--- src/main/java/com/koreait/surl_project_11/surlProject11Application.java
+++ src/main/java/com/koreait/surl_project_11/surlProject11Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SurlProject11Application {
+public class surlProject11Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SurlProject11Application.class, args);
+        SpringApplication.run(surlProject11Application.class, args);
     }
 
 }
